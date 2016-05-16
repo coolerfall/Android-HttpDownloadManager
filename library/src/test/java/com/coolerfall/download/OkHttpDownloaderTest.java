9eb41688f4bf89c15edfcb7ee35a7146fc33fa30
Vincent Cheung <coolingfall@gmail.com>
2016-05-16 16:42:19 +0800

    Add unit test

---
+++ library/src/test/java/com/coolerfall/download/OkHttpDownloaderTest.java
@@ -0,0 +1,87 @@
+package com.coolerfall.download;
+
+import android.net.Uri;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+
+/**
+ * @author Vincent Cheung (coolingfall@gmail.com)
+ */
+@RunWith(RobolectricGradleTestRunner.class) @Config(constants = BuildConfig.class, sdk = 21)
+public class OkHttpDownloaderTest {
+	private static final int CONTENT_LENGTH = 1024 * 1024 * 5;
+	private Uri mockUri;
+	private MockWebServer mockWebServer;
+	private OkHttpDownloader okHttpDownloader;
+
+	@Before public void setUp() throws Exception {
+		mockWebServer = new MockWebServer();
+		mockUri = Uri.parse(mockWebServer.url("/").toString());
+		okHttpDownloader = OkHttpDownloader.create();
+	}
+
+	@After public void close() throws Exception {
+		okHttpDownloader.close();
+	}
+
+	@Test public void testFullDownload() throws Exception {
+		MockResponse response = new MockResponse().setHeader("Content-Length", CONTENT_LENGTH);
+		mockWebServer.enqueue(response);
+		okHttpDownloader.start(mockUri, 0);
+		assertEquals(CONTENT_LENGTH, okHttpDownloader.contentLength());
+	}
+
+	@Test public void testBreakpointDownload() throws Exception {
+		MockResponse response =
+			new MockResponse().setHeader("Content-Length", CONTENT_LENGTH - 2000000);
+		mockWebServer.enqueue(response);
+		okHttpDownloader.start(mockUri, 2000000);
+		assertEquals(CONTENT_LENGTH - 2000000, okHttpDownloader.contentLength());
+	}
+
+	@Test public void testCustomClient() throws Exception {
+		OkHttpClient client = new OkHttpClient.Builder().build();
+		OkHttpDownloader downloader = OkHttpDownloader.create(client);
+		MockResponse response = new MockResponse().setHeader("Content-Length", CONTENT_LENGTH);
+		mockWebServer.enqueue(response);
+		downloader.start(mockUri, 0);
+		assertEquals(CONTENT_LENGTH, downloader.contentLength());
+		assertNotNull(downloader.byteStream());
+	}
+
+	@Test public void testRedirection() throws Exception {
+		MockResponse response =
+			new MockResponse().setResponseCode(301).addHeader("Location", mockUri.toString());
+		mockWebServer.enqueue(response);
+		response =
+			new MockResponse().setResponseCode(200).setHeader("Content-Length", CONTENT_LENGTH);
+		mockWebServer.enqueue(response);
+		okHttpDownloader.start(mockUri, 0);
+		assertEquals(CONTENT_LENGTH, okHttpDownloader.contentLength());
+	}
+
+	@Test public void testBadUrl() {
+		try {
+			okHttpDownloader.start(Uri.parse("www.baidu.com"), 0);
+		} catch (IOException e) {
+			if (e instanceof DownloadException) {
+				assertSame("url should start with http or https", e.getMessage());
+			}
+		}
+	}
+}
